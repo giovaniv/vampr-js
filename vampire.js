e34e56e40156b6ab1ff9b11f25e5f5573dc34f89
giovaniv <giovaniv@gmail.com>
2018-07-06 14:39:35 +0000

    update

---
+++ vampire.js
@@ -0,0 +1,73 @@
+class Vampire {
+  constructor(name, yearConverted) {
+    this.name = name;
+    this.yearConverted = yearConverted;
+    this.offspring = [];
+    this.creator = null;
+  }
+
+  /** Simple tree methods **/
+
+  // Adds the vampire as an offspring of this vampire
+  addOffspring(vampire) {
+    this.offspring.push(vampire);
+    vampire.creator = this;
+  }
+
+  // Returns the total number of vampires created by that vampire
+  get numberOfOffspring() {
+    return this.offspring.length;
+  }
+
+  // Returns the number of vampires away from the original vampire this vampire is
+  get numberOfVampiresFromOriginal() {
+    if (!this.creator) {
+      return 0;
+    } else {
+      let total = 0;
+      let currentVampire = this;
+      while (currentVampire.creator) {
+        currentVampire = currentVampire.creator;
+        total++;
+      }
+      return total;
+    }
+  }
+
+  // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
+  isMoreSeniorThan(vampire) {
+    if (!this.creator) {
+      return true;
+    }
+    if (!vampire.creator) {
+      return false;
+    }
+    if (this.offspring.indexOf(vampire)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  /** Stretch **/
+
+  // Returns the closest common ancestor of two vampires.
+  // The closest common anscestor should be the more senior vampire if a direct ancestor is used.
+  // For example:
+  // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
+  // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
+  closestCommonAncestor(vampire) {
+    if (!this.creator) {
+      return this;
+    } else {
+      if (!vampire.creator) {
+        return vampire;
+      } else {
+        return 'i need to finish...';
+      }
+    }
+  }
+}
+
+module.exports = Vampire;
+
